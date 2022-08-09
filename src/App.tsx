4229ed167df1e8c958d082e2a1cd634ae1ed10c4
Darya Zata <darya.zata@outlook.com>
2022-08-09 16:26:06 +0200

    feat: edit app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import AboutComponent from './components/About';
+import AppBar from './components/AppBar';
+import Footer from './components/Footer';
+import HomeComponent from './components/Home';
+import ProjectsComponent from './components/Projects';
+import ToTopComponent from './components/ToTopComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <AppBar />
+      <HomeComponent />
+      <AboutComponent />
+      <ProjectsComponent />
+      <ToTopComponent />
+      <Footer />
     </div>
   );
 }
