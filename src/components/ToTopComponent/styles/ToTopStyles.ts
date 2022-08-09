72b631a05c5ec4e32c11486e73d331ebda0bcf9f
Darya Zata <darya.zata@outlook.com>
2022-08-09 17:10:36 +0200

    feat: add to-top component

---
+++ src/components/ToTopComponent/styles/ToTopStyles.ts
@@ -0,0 +1,16 @@
+import { COLORS } from '../../../globalStyles/colors';
+
+export const styleContainerBox = {
+  backgroundColor: COLORS.THEME_YELLOW_TRANS,
+  position: 'fixed',
+  bottom: 0,
+  right: 0,
+  p: 1.3,
+  px: 1.5,
+  borderRadius: 15,
+  m: 3,
+  color: COLORS.WHITE,
+  ':active': {
+    color: COLORS.WHITE,
+  },
+};
