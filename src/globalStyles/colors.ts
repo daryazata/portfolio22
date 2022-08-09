14d8ae0a5a01f6b9a1b9fc90da3d8d90efe5f3dd
Darya Zata <darya.zata@outlook.com>
2022-08-09 16:45:15 +0200

    feat: add global colors

---
+++ src/globalStyles/colors.ts
@@ -0,0 +1,12 @@
+export enum COLORS {
+  THEME_YELLOW = '#ebed72',
+  THEME_YELLOW_TRANS = 'rgba(235, 237, 114, 0.85)',
+  GRAY_1 = '#9e9e9e',
+  GRAY_2 = '#cccccc',
+  GRAY_3 = '#191919',
+  THEME_RED = '#f5024d',
+  THEME_BLUE = '#1d91eb',
+  BLUE_2 = '#74b6cd',
+  WHITE = '#FFFFFF',
+  THEME_BG_BLACK = '#090909',
+}
