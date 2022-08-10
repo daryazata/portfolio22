cdee25f477f98d2eb952b6624badbd3c167f6167
Darya Zata <darya.zata@outlook.com>
2022-08-10 15:54:14 +0200

    chore(home): edit mobile animation

--- src/components/Home/styles/stylesHome.ts
+++ src/components/Home/styles/stylesHome.ts
@@ -18,25 +18,29 @@ export const WelcomeTextName = styled.p`
   letter-spacing: 0.2rem;
 `;
 
-const vanishToTop = keyframes`
-0% { opacity:1.0 }
-30% { opacity:1.0 }
-75% { transform : translateY(-15px);
-    opacity:0.0 }
-100% {opacity:0} `;
+const vanishToTop = (translateY: number) => keyframes`
+  0% { opacity:1.0 }
+  30% { opacity:1.0 }
+  75% { transform : translateY(-${translateY}px);
+      opacity:0.0 }
+  100% {opacity:0} `;
 
-const vanishToTop2 = keyframes`
-0% { opacity:0.0 }
-30% { transform : translateY(-15px); opacity:0.5 }
-60% { opacity:1.0; transform : translateY(15px); }
-100% { opacity:1.0 } `;
+const vanishToTop2 = (translateY: number) => keyframes`
+  0% { opacity:0.0 }
+  30% { transform : translateY(-${translateY}px); opacity:0.5 }
+  60% { opacity:1.0; transform : translateY(${translateY}px) }
+  100% { opacity:1.0 } `;
 
 export const WelcomeTextDeveloper = styled.p`
   font-family: 'Bungee', cursive;
   color: ${COLORS.THEME_BLUE};
   line-height: 1.2rem;
   letter-spacing: 0.25rem;
-  animation: ${vanishToTop} 2s linear infinite;
+  animation: ${vanishToTop(15)} 2s linear infinite;
+
+  @media (max-width: 770px) {
+    animation: ${vanishToTop(10)} 2s linear infinite;
+  }
 `;
 
 export const WelcomeTextDeveloper2 = styled.p`
@@ -44,12 +48,13 @@ export const WelcomeTextDeveloper2 = styled.p`
   color: ${COLORS.THEME_YELLOW};
   line-height: 1.2rem;
   letter-spacing: 0.25rem;
-  animation: ${vanishToTop2} 5s linear infinite;
+  animation: ${vanishToTop2(15)} 5s linear infinite;
   position: absolute;
-  bottom: 240px;
+  bottom: 222px;
   opacity: 0;
   @media (max-width: 770px) {
-    bottom: 105px;
+    bottom: 111px;
+    animation: ${vanishToTop2(10)} 5s linear infinite;
   }
 `;
 
@@ -60,7 +65,7 @@ export const WelcomeTextBase = styled.p`
   letter-spacing: 0.25rem;
   margin-top: -30px;
   @media (max-width: 770px) {
-    margin-top: -5pxs;
+    margin-top: -25px;
   }
 `;
 
